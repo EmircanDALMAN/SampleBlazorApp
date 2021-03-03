1cdf4557d71ffb16f047ed1f5645678f73b02634
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-03-03 14:45:22 +0300

    Add project files.

---
+++ DemoBlazor/Services/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using DemoBlazor.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazor.Services
+{
+    public class CategoryService:ICategoryService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryService(HttpClient http)
+        {
+            _httpClient = http;
+        }
+        public Task<Category[]> GetCategories()
+        {
+            return _httpClient.GetJsonAsync<Category[]>("https://localhost:44335/api/categories/getall");
+        }
+    }
+}
---
+++ DemoBlazor/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Blazor.Extensions.Storage;
+using DemoBlazor.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazor.Services
+{
+    public class ProductService:IProductService
+    {
+        private HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            
+        }
+
+        
+        public Task<Product[]> GetProducts()
+        {
+            return _httpClient.GetJsonAsync<Product[]>("https://localhost:44335/api/products/getall");
+        }
+
+        public Task<Product> GetProductById(int productId)
+        {
+            return _httpClient.GetJsonAsync<Product>("https://localhost:44335/api/products/getbyid?productId="+productId);
+        }
+
+        public async Task Save(Product product)
+        {
+            await _httpClient.PostJsonAsync("https://localhost:44335/api/products/update", product);
+        }
+
+        public async Task Add(Product product)
+        {
+            
+            await _httpClient.PostJsonAsync("https://localhost:44335/api/products/add", product);
+        }
+    }
+}
